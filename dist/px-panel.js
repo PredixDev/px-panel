779c69a0740e63f04724335a1772cb93e4935d59
Randy Askin <randall.askin@ge.com>
2017-10-20 10:24:04 -0700

    initial release

---
+++ dist/px-panel.js
@@ -0,0 +1,33 @@
+'use strict';(function(){Polymer({is:'px-panel',behaviors:[Polymer.IronResizableBehavior],listeners:{'iron-resize':'_onResize'},properties:{/**
+      * Where to place the panel - one of `top`, `bottom`, `left`, or `right`.
+      */position:{type:String,value:'right',reflectToAttribute:true,observer:'_onResize'},/**
+       * Whether the panel is currently open (expanded).
+       */opened:{type:Boolean,value:false,notify:true,reflectToAttribute:true},/**
+       * If set to true, the panel will have `position:fixed` so it will
+       * be attached to the browser window instead of its parent container.
+       */fixed:{type:Boolean,value:false,reflectToAttribute:true,observer:'_onResize'},/**
+       * If set to true, the panel will be opened and calls to the `close()` method will be ignored.
+       * Mutating the `opened` property will still force a close of the panel.
+       */persistent:{type:Boolean,value:false,observer:'_persistentChanged'},/**
+       * Whether to display the panel with a `light`, `medium`, or `dark` background.
+       * These terms are relative, and can be used in conjunction with px-theme,
+       * px-dark-theme, or your custom theme or CSS variables to toggle between 3 different
+       * background colors.
+       */background:{type:String,value:'light',reflectToAttribute:true},/**
+       * If set to true, the panel will appear with an offset relative to the screen / container edges.
+       * Can be overridden or customized using the CSS variables.
+       */floating:{type:Boolean,value:false,reflectToAttribute:true},/**
+       * If set to true, the panel will not fully collapse when closed. It will appear in a minimized state.
+       * You can use the content slot `minimized` to determine what will appear inside the panel while minimized
+       * (likely an icon or button for fully opening the panel).
+       */minimizable:{type:Boolean,value:false,reflectToAttribute:true},/**
+       * Used internally to determine if the panel should display at full-width or full-height
+       * for mobile responsiveness and space-constrained situations.
+       */fullSize:{type:Boolean,value:false,reflectToAttribute:true,readOnly:true}},/**
+    * Opens the panel
+    */open:function open(){this.opened=true},/**
+    * Closes the panel
+    */close:function close(){if(!this.persistent){this.opened=false}},/**
+     * Sets the `opened` property for persistent panels.
+     */_persistentChanged:function _persistentChanged(newValue){if(newValue&&!this.opened){this.open()}},_onResize:function _onResize(e){this.debounce('resize',function(){if(this.fixed){if((this.position==='left'||this.position==='right')&&window.innerWidth<600||(this.position==='top'||this.position==='bottom')&&window.innerHeight<600){this._setFullSize(true)}else{this._setFullSize(false)}}else{if((this.position==='left'||this.position==='right')&&this.parentNode.getBoundingClientRect().width<600||(this.position==='top'||this.position==='bottom')&&this.parentNode.getBoundingClientRect().height<600){this._setFullSize(true)}else{this._setFullSize(false)}}},100)}})})();
+//# sourceMappingURL=px-panel.js.map
---
+++ test/px-panel-custom-tests.js
@@ -0,0 +1,122 @@
+document.addEventListener("WebComponentsReady", function () {
+  runCustomTests();
+});
+
+function runCustomTests() {
+
+  describe('Basic tests for px-panel', function() {
+    var panelFixture, panel;
+
+    beforeEach(function() {
+      panelFixture = fixture('panelFixture'),
+      panel = panelFixture.querySelector('#panel');
+      // disable animations to avoid need for setTimeouts
+      panel.style.transition = "none";
+    });
+
+    it('has default settings', function(){
+      expect(panel.opened).to.be.false;
+      expect(panel.fixed).to.be.false;
+      expect(panel.persistent).to.be.false;
+      expect(panel.floating).to.be.false;
+      expect(panel.minimizable).to.be.false;
+    });
+    it('is hidden by default', function(done) {
+      flush(function() {
+        var width = parseFloat(window.getComputedStyle(panel).getPropertyValue('width')),
+            height = parseFloat(window.getComputedStyle(panel).getPropertyValue('height'));
+        expect(width).to.equal(0);
+        expect(height).to.be.closeTo(300,1);
+        done();
+      });
+    });
+    it('is empty by default', function(done) {
+      flush(function() {
+        var contents = panel.innerText.trim();
+        expect(contents).to.equal('');
+        done();
+      });
+    });
+    it('expands with .open()', function(done){
+      panel.open();
+      expect(panel.opened).to.be.true;
+      flush(function() {
+        var width = parseFloat(window.getComputedStyle(panel).getPropertyValue('width')),
+            height = parseFloat(window.getComputedStyle(panel).getPropertyValue('height'));
+        expect(width).to.be.closeTo(320,1);
+        expect(height).to.be.closeTo(300,1);
+        done();
+      });
+    });
+    it('expands with opened property', function(done){
+      panel.opened = true;
+      flush(function() {
+        var width = parseFloat(window.getComputedStyle(panel).getPropertyValue('width')),
+            height = parseFloat(window.getComputedStyle(panel).getPropertyValue('height'));
+        expect(width).to.be.closeTo(320,1);
+        expect(height).to.be.closeTo(300,1);
+        done();
+      });
+    });
+    it('works as expected with persistent', function(){
+      panel.persistent = true;
+      expect(panel.opened).to.be.true;
+      panel.close();
+      expect(panel.opened).to.be.true;
+    });
+    it('displays content when opened', function(done) {
+      panel.open();
+      flush(function() {
+        var contents = panel.innerText.trim();
+        expect(contents).to.equal('Test');
+        done();
+      });
+    });
+    it('appears when minimized', function(done){
+      panel.minimizable = true;
+      flush(function() {
+        var width = parseFloat(window.getComputedStyle(panel).getPropertyValue('width')),
+            height = parseFloat(window.getComputedStyle(panel).getPropertyValue('height'));
+        expect(width).to.equal(60);
+        expect(height).to.be.closeTo(300,1);
+        done();
+      });
+    });
+    it('displays minimized content', function(done) {
+      panel.minimizable = true;
+      flush(function() {
+        var contents = panel.innerText.trim();
+        expect(contents).to.equal('Mini');
+        done();
+      });
+    });
+    it('works as expected with fixed', function(done) {
+      panel.fixed = true;
+      panel.open();
+      flush(function() {
+        var width = parseFloat(window.getComputedStyle(panel).getPropertyValue('width')),
+            height = parseFloat(window.getComputedStyle(panel).getPropertyValue('height')),
+            docHeight = document.documentElement.clientHeight;
+        expect(width).to.be.closeTo(320,1);
+        expect(height).to.equal(docHeight);
+        done();
+      });
+    });
+    it('behaves responsively', function(done) {
+      panelFixture.style.width = '500px';
+      panel.open();
+      panel.notifyResize();
+      flush(function() {
+        window.setTimeout(function() {
+          var width = parseFloat(window.getComputedStyle(panel).getPropertyValue('width')),
+              height = parseFloat(window.getComputedStyle(panel).getPropertyValue('height'));
+          expect(width).to.equal(500);
+          expect(height).to.equal(300);
+          done();
+        },150); // account for resize debounce
+      });
+    });
+
+
+  });
+}
